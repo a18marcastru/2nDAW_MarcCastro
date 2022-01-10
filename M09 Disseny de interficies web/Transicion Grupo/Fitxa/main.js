f92d8e5316e8f07137a1371c635859ac1c745a1a
a18marcastru <a18marcastru@inspedralbes.cat>
2022-01-10 14:22:48 +0100

    Transicion grupo

---
+++ M09 Disseny de interficies web/Transicion Grupo/Fitxa/main.js
@@ -0,0 +1,10 @@
+document.getElementById("cambiar").addEventListener('click', function(){
+    if(document.getElementById("fitxa") != )
+    text = "";
+    text +=`<p>Nombre: Marc</p>
+            <p>Edat: 20</p>`;
+    document.getElementById("fitxa").innerHTML = text;
+});
+document.getElementById("cambiar").addEventListener('click', function(){
+    document.getElementById("fitxa").setAttribute("style", "background: red;");
+});
