c40dd150efb942ebc2b8ceba9a2938918a7e95c4
a18marcastru <a18marcastru@inspedralbes.cat>
2022-02-08 10:20:58 +0100

    Persona JavaDocs

--- M05 Entorns de desenvolupament/UF2/CalculoTest/src/unit/test/classes/MultiplicaCastroMarc.java
+++ M05 Entorns de desenvolupament/UF2/CalculoTest/src/unit/test/classes/MultiplicaCastroMarc.java
@@ -1,6 +1,6 @@
 package unit.test.classes;
 
-public class MultiplicaCognomNom {
+public class MultiplicaCastroMarc {
 	public int multiply(int number1, int number2) {
 		return number1 * number2;
 	}
---
+++ M05 Entorns de desenvolupament/UF2/PersonaTest/src/Persona.java
@@ -0,0 +1,87 @@
+/**
+ * Esta clase esta enfocado en trabajar con la informacion del usuario. 
+ * @author Marc
+ * @version 2022
+ */
+public class Persona {
+	/**
+	 * Aqui estan los atributos que contendran la informacion del usuario de manera privada.
+	 */
+	private String dni;
+	private String nom;
+	private String cognom;
+	private int edat;
+	private boolean estatCivil;
+	
+	/**
+	 * Aqui estan los constructores que gestionaran la informacion recibida con los parametros que establecemos.
+	 * @param edat = 10 tiene la atributo definido en caso de no tener la informacion de la edat, se pondra el numero 10.
+	 */
+	Persona(){
+		this.edat=10;
+	}
+	Persona(String nom, String cognom){
+		this.nom=nom;
+		this.cognom=cognom;
+	}
+	Persona(String dni, String nom, String cognom, int edat, boolean estat){
+		this.dni=dni;
+		this.nom=nom;
+		this.cognom=cognom;
+		this.edat=edat;
+		this.estatCivil=estat;
+	}
+	
+	/**
+	 * Los Setters y Getters para acceder a los atributos privados para poder modificar la informacion y devolverlo.
+	 * @param dni en el public void setDni(String dni) es donde recibe la informacion y lo introduce al atributo privado.
+	 * @return dni en el public String getDni() devuelve la informacion introducido.
+	 */
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public int getEdat() {
+		return edat;
+	}
+	public void setEdat(int edat) {
+		this.edat = edat;
+	}
+	public String getCognom() {
+		return cognom;
+	}
+	public void setCognom(String cognom) {
+		this.cognom = cognom;
+	}
+	public boolean isEstatCivil() {
+		return estatCivil;
+	}
+	public void setEstatCivil(boolean estatCivil) {
+		this.estatCivil = estatCivil;
+	}
+	public void Aniversari() {
+		this.edat++;
+	}
+	public void Casament() {
+		this.estatCivil=true;
+	}
+	public void Divorci() {
+		this.estatCivil=false;
+	}
+	public String toString() {
+		return ("Soc " + this.nom + " " +
+		this.cognom
+		+ " amb DNI" + this.dni
+		+ " tinc " + this.edat + " anys i "
+		 + (this.estatCivil==true?" SI ":" NO ")
+		 + " estic casat");
+	}
+}
