15b588432dfccb8f1bbfd239e7a8e9600221530a
a18marcastru <a18marcastru@inspedralbes.cat>
2022-02-11 13:27:10 +0100

    VUE y media query

---
+++ M06 Desenvolupament Web en entorn Client/VUE.JS/vue-project/src/App.vue
@@ -0,0 +1,55 @@
+<script>
+  import Gato from './components/Gato.vue';
+
+  export default {
+    components: {
+      Gato
+    },
+    data() {
+      return {
+        catActive: 0,
+        gatos: [
+          { name: "Michifu", content: "Mi gato Michifu", image: "./public/img/cat.jpg", nclicks: 0 },
+          { name: "Minino", content: "Mi gato Minino", image: "./public/img/cat2.jpg", nclicks: 0 },
+          { name: "Garfield", content: "Mi gato Garfield", image: "./public/img/cat3.jpg", nclicks: 0 },
+          { name: "Roberto", content: "Mi gato Roberto", image: "./public/img/cat4.jpg", nclicks: 0 }
+        ]
+      }
+    }
+  }
+</script>
+
+<template>
+  <header>
+    <br>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  </header>
+  <main>
+    <div id="app">
+        <ul>
+            <li :class="{active: catActive==index}" @click="cambioGatos(index)" :key=index v-for="(item, index) in gatos">
+                {{item.name}}
+            </li>
+        </ul>
+
+        <div>
+          <hr>
+          <Gato :infoGato="gatos[catActive]"
+          @like="gatos[catActive].nclicks++"
+          @dislike="gatos[catActive].nclicks--"/>
+        </div>
+    </div>
+  </main>
+</template>
+
+<style>
+@import './assets/base.css';
+.active {
+  color: red;
+}
+img {
+  width: 300px;
+}
+
+
+</style>
---
+++ M06 Desenvolupament Web en entorn Client/VUE.JS/vue-project/src/components/Gato.vue
@@ -0,0 +1,20 @@
+<!-- Gato.vue -->
+<script>
+export default {
+  props: ['infoGato'],
+  emits: ['incrementar','like','dislike']
+}
+</script>
+
+<template>
+    <div>
+        <h1>Hola somos gatos componente</h1>
+        <br>
+        <h3>{{infoGato.content}}</h3>
+        <h3>{{infoGato.nclicks}}{{infoGato.nclicks != 1 ? " clicks" : " click"}}</h3>
+        <button class="btn btn-primary" @click="$emit('like')">Like</button>
+        <button class="btn btn-danger" @click="$emit('dislike')">Dislike</button>
+        <br>
+        <img :src="infoGato.image" alt=""/>
+    </div>
+</template>
